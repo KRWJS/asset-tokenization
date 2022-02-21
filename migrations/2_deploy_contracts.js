f8465d9f39b25f58ff9f1519bb0f692901e9133c
Kyle <me@kylewilliams.co.uk>
2022-02-21 09:07:09 +0800

    Add contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
-var MyToken = artifacts.require('MyToken.sol');
+var MyToken = artifacts.require('MyToken');
+var MyTokenSales = artifacts.require('./MyTokenSale');
 
 module.exports = async function (deployer) {
+	let addr = await web3.eth.getAccounts();
 	await deployer.deploy(MyToken, 7777777);
+	await deployer.deploy(MyTokenSales, 1, addr[0], MyToken.address);
+	let tokenInstance = await MyToken.deployed();
+	await tokenInstance.transfer(MyTokenSales.address, 7777777);
 };
